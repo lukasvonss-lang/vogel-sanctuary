c1f63c6702e7db6befa150fabf291b5e10b4ef3f
lukasvonss-lang <lukasvonss@gmail.com>
2026-09-18 20:27:50 -0600

    Update generate.rb

--- scripts/generate.rb
+++ scripts/generate.rb
@@ -1,20 +1,16 @@
-require 'json'
-require 'fileutils'
+Dir.glob("_pics/*.{jpg,jpeg,png,webp,JPG,PNG}").each do |path|
+  id = File.basename(path, ".*")
+  out = "_posts/2000-01-01-#{id}.md"
+  next if File.exist?(out)
 
-pics_dir = "_pics"
-posts_dir = "_posts"
-FileUtils.mkdir_p(posts_dir)
-
-Dir.glob("#{pics_dir}/*.{jpg,jpeg,png,webp,gif}").each do |img_path|
-  name = File.basename(img_path, ".*")
-  next if File.exist?("#{posts_dir}/#{name}.md") # don't overwrite
-
-  File.write("#{posts_dir}/#{Time.now.strftime('%Y-%m-%d')}-#{name}.md", <<~MD)
+  File.write(out, <<~MD)
     ---
-    layout: pic
-    title: "#{name.gsub('-',' ').capitalize}"
-    image_path: "/#{img_path}"
-    date: #{Time.now.strftime('%Y-%m-%d %H:%M:%S %z')}
+    layout: post
+    post_id: post#{id}
+    title: "Art ##{id}"
+    image: "/#{path}"
+    thumbnail: "/Thumbnails/#{id}.jpg"
+    description: "No description yet"
     ---
   MD
 end
